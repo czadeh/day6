de1a472e8648e53fec00ea36c19e7cf95106e20f
Your Name <cameron>
2021-08-08 20:15:48 -0400

    mvp features

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
-import React from "react";
-import Example from "./components/Example";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import { AllHtmlEntities } from "html-entities";
+import Button from "./components/Button";
+import "./App.css"
+
+const entities = new AllHtmlEntities();
+
 const App = () => {
+  const [question, setQuestion] = useState(null);
+  const [answer, setAnswer] = useState(null);
+  const [streak, setStreak] = useState(0);
+  const [attempts, setAttempts] = useState(0);
+  const [correct, setCorrect] = useState(0);
+
+  const onAnswer = (torF) => {
+    if (torF  === answer) {
+      setStreak(() => (streak + 1));
+      setCorrect(() => (correct + 1));
+    }
+    else {
+      setStreak(0);
+    }
+    setAttempts(attempts + 1);
+  }
+
+  useEffect(() => {
+    axios.get("https://opentdb.com/api.php?amount=1&type=boolean").then((response) => {
+      const result = response.data.results[0];
+      setQuestion(result.question);
+      setAnswer(result.correct_answer);
+    });
+  }, [attempts]);
+
   return (
-    <div className="w-full h-screen flex flex-row justify-center">
-      <div className="flex flex-col justify-center">
-        <Example label="Tracecamp CRA Template" />
+    <div className="top">
+      <div className="grid-rows-3">
+        <div>
+          {entities.decode(question)}
+        </div>
+        <div className = "flex-row flex justify-center pt-10">
+          <Button onClick={() => onAnswer('True')}>True</Button>
+          <Button onClick={() => onAnswer('False')}>False</Button>
+        </div>
+        <div>
+          Streak: {streak} <br />
+          Attemps: {attempts} <br />
+          Correct Answers: {correct}
+        </div>
       </div>
     </div>
   );
